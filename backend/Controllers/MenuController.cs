30b6603b6e68bbff748ac3ba1a1c3fd33593db5c
Jomari Marson <jomari.marson@gmail.com>
2025-09-19 18:33:33 +0800

    adding the route

--- backend/Controllers/MenuController.cs
+++ backend/Controllers/MenuController.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 namespace backend.Controllers
 {
         [ApiController]
-        [Route("api/[controller]")]
+        [Route("api/menu")]
         public class MenuController : ControllerBase
         {
             private readonly ApplicationDBContext _context;
